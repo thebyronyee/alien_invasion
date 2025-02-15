8d0fcbdae94df0bc74251f85ddcfda96feb8ac28
thebyronyee <thebyronyee@gmail.com>
2025-02-15 13:49:36 +0800

    Modifying ship programming based on 3rd ed of Python Crash Course

--- game_functions.py
+++ game_functions.py
@@ -2,14 +2,18 @@ import sys
 import pygame
 
 def check_events():
-    """Respond to keypresses and mouse events."""
-    for event in pygame.event.get():
+   """Respond to keypresses and mouse events."""
+   for event in pygame.event.get():
         if event.type == pygame.QUIT:
-                sys.exit()
+            sys.exit()
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RIGHT:
+                #Move the ship to the right
+                ship.rect.centerx += 1
 
 def update_screen(ai_settings, screen, ship):
      """Update images on the screen and flip to the new screen."""
-    # Redraw screen during each pass through loop.
+    #Redraw screen during each pass through loop.
     screen.fill(ai_settings.bg_color)
     ship.blitme()
      
--- ship.py
+++ ship.py
@@ -1,19 +1,19 @@
 import pygame
 
 class Ship():
-    
-    def __init__(self, screen):
+    """A class to manage the ship."""
+    def __init__(self, ai_game):
         """Initialize the ship and set its starting position."""
-        self.screen = screen
+        self.screen = ai_game.screen
+        self.screen_rect = ai_game.screen.get_rect()
 
         #Load the ship image and get its rect.
-        self.image = pygame.image.load('Documents/python/alien_invasion/images/ship.bmp')
+        self.image = pygame.image.load('/home/thebyronyee/Documents/python/alien_invasion/images/ship.bmp')
         self.rect = self.image.get_rect()
-        self.screen_rect = screen.get_rect()
+        #self.screen_rect = screen.get_rect()
 
         #Start each new ship at the bottom center of the screen.
-        self.rect.centerx = self.screen_rect.centerx
-        self.rect.bottom = self.screen_rect.bottom
+        self.rect.midbottom = self.screen_rect.midbottom
 
     def blitme(self):
         """Draw the ship at its current location."""
